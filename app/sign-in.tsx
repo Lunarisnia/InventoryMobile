1fda937e3a43dd1d8684b0ebc487806e8ff033a5
louna <r.arswendo.r@gmail.com>
2024-12-12 21:18:18 +0700

    wip: added sign in nis and password field

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,22 +1,50 @@
 import { useSession } from '@/components/AuthContext';
 import { router } from 'expo-router';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Dimensions, StyleSheet, Text, TextInput, useWindowDimensions, View } from 'react-native';
 
 
 // TODO: Create a proper login page with text input for NIS and password
 export default function SignIn() {
   const { signIn } = useSession();
+  const [nis, setNis] = useState("123123");
+  const [password, setPassword] = useState("fpppfppfpf");
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text
+      <Text>Inventory Manager</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setNis}
+        value={nis}
+      />
+      <TextInput
+        textContentType='password'
+        secureTextEntry={true}
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <Button title="Login"
         onPress={() => {
           signIn();
-          // Navigate after signing in. You may want to tweak this to ensure sign-in is
-          // successful before navigating.
           router.replace('/');
-        }}>
-        Sign In
-      </Text>
+        }} />
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    width: Dimensions.get("window").width - 20,
+  },
+  button: {
+    borderWidth: 1,
+    padding: 10,
+  }
+});
